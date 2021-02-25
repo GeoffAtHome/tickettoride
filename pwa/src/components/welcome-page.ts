e7ac49932a7f427c634537ab130f987b33c5bbb0
GeoffAtHome <geoff@soord.org.uk>
2021-02-25 14:06:32 +0000

    Correct instructions.

--- pwa/src/components/welcome-page.ts
+++ pwa/src/components/welcome-page.ts
@@ -33,23 +33,29 @@ export class WelcomePage extends PageViewElement {
       <h1>Ticket to Ride cards for the game v0.0.1</h1>
       <h3>Background</h3>
       <p>
-        This was writen to allow to Ticket to Ride to be played remotely during
-        the pandemic (2020 -2021). You still require the board, trains and
-        stations to play the game. This PWA manages the deck of cards, discard
-        pile and the players hand.
+        This was writen to allow 'Ticket to Ride' to be played remotely during
+        the pandemic (2020 -2021). You still require the game for the board,
+        trains and stations to play the game.
+      </p>
+      <p>
+        This web app manages the deck of cards, discard pile and the players
+        hand.
       </p>
       <h3>Starting the game.</h3>
       <p>
-        Select 'START' from the menu and enter your name 'player' and the name
-        of the game 'game' and the click 'ADD PLAYER'. Other people playing do
-        the same from their phones, tablets or other devices. When all players
-        who want to play are listed Someone presses 'START GAME' to create the
-        game to be played. Following this, everyone presses 'GO TO GAME'
+        Select 'START' from the menu and enter your name 'player', the game name
+        'game' and then click 'ADD PLAYER'. Other people playing do the same
+        from their phones, tablets or other devices. When all players are listed
+        someone presses 'START GAME' to create the game to be played. Following
+        this, everyone presses 'GO TO GAME'
       </p>
       <p>
         IMPORTANT: The 'game' name is important. Using a different name from
         other players will mean each player will be in a different game.
       </p>
+      The play order is the same as players are entered. Currently this cannot
+      be changed.
+      <p></p>
       <p>
         Pressing 'START GAME' will reset the game so avoid pressing this
         mid-game.
